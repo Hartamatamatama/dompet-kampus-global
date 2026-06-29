74ba69fae30e473036f66ec5e112698d11a4a8a6
hartamatamatama <katonojutsu340@gmail.com>
2026-06-29 13:46:04 +0700

    docs(utils): add doc comment to AppBlocObserver

--- lib/core/utils/app_bloc_observer.dart
+++ lib/core/utils/app_bloc_observer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/foundation.dart';
+/// Custom BlocObserver that logs all state transitions and events for debugging.
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AppBlocObserver extends BlocObserver {
